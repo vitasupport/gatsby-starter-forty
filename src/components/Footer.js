e7ef03cdaa62ef73e698176df6e4664a0f1f58dc
groninge01 <groninge@hotmail.com>
2020-07-07 16:30:52 +0200

    update adres gegevens

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,23 @@ const Footer = props => (
     <div className="inner">
       <div className="grid-wrapper">
         <div className="col-4">
-          <p>Adresgegevens?</p>
+          <p>VitaSupport</p>
+          <p>Koelmalaan 330</p>
+          <p>1812 PS Alkmaar</p>
+          <p>T: 06 22445302</p>
+          <p>
+            E: <Link to="/contact">contact</Link>
+          </p>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2422.323597073319!2d4.7611640156370125!3d52.61799613671469!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47cf563020ec9807%3A0xd5e7f25e368e844!2sVitaSupport!5e0!3m2!1snl!2snl!4v1594130711193!5m2!1snl!2snl"
+            width="300"
+            height="225"
+            frameborder="0"
+            style={{ border: 0 }}
+            allowfullscreen=""
+            aria-hidden="false"
+            tabindex="0"
+          ></iframe>
         </div>
         <div className="col-4">
           <ul className="alt links">
