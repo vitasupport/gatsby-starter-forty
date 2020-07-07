af63dab443c1302de027a0914550509a148bd2d5
groninge01 <groninge@hotmail.com>
2020-07-07 16:31:10 +0200

    add contact form WIP

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,7 +12,44 @@ const ContactPage = props => (
           <header className="major">
             <h1>Contact</h1>
           </header>
-          Inhoud?
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            data-netlify-recaptcha="true"
+          >
+            {/* You still need to add the hidden input with the form name to your JSX form */}
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="field">
+              <label htmlFor="name">Naam</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="field">
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="phonenumber">Telefoonnummer</label>
+              <input type="text" name="phonenumber" id="phonenumber" />
+            </div>
+            <div className="field">
+              <label htmlFor="message">Bericht</label>
+              <textarea name="message" id="message" rows="6"></textarea>
+            </div>
+            <div data-netlify-recaptcha="true"></div>
+            <ul className="actions">
+              <li>
+                <input
+                  type="submit"
+                  value="Verstuur bericht"
+                  className="special"
+                />
+              </li>
+              <li>
+                <input type="reset" value="Reset" />
+              </li>
+            </ul>
+          </form>
         </div>
       </section>
     </div>
